806f5c63b803cbcf7a8725eb0ae1c08d42966879
jalkiechev <157492311+jalkiechev@users.noreply.github.com>
2024-01-23 16:02:20 +0300

    классная работа 23.01.2024

---
+++ main.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+print("Привет мир") #вывод
+input() #ввод 
+total = imet1 + \
+        item2 + \
+        item3
+days = ["Sunday", "Monday"]
+import random; x = random.randint();
+a = "2"
+b = "3"
+print(a+b)
